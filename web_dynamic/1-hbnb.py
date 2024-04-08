6205f4247f73bd3c3fac5c2aea34ec7b1d01568a
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-04-08 16:51:48 +0000

    Fix selecting amenities (Task 2)

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -8,6 +8,7 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
+
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -41,7 +42,6 @@ def hbnb():
                            places=places,
                            cache_id=uuid.uuid4())
 
-
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5000)
--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,16 @@
-let amenities = {};
-document.addEventListener("DOMContentLoaded", function () {
-    $("input:checkbox").change(function () {
-        if ($(this).is(':checked')) {
-            amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-        }  else {
-            delete amenities[$(this).attr('data-id')];
-        }
-        $('DIV.amenities h4').text(Object.values(amenities).join(', '));
+$(function () {
+  let selectedAmenities = {}
+  $('.filters .amenities input[type="checkbox"]').on('change', function () {
+    // update selected amenities dictionnary
+    amenityId = $(this).attr('data-id')
+    amenityName = $(this).attr('data-name')
+    if ($(this).is(':checked'))
+      selectedAmenities[amenityId] = amenityName;
+    else
+      delete selectedAmenities[amenityId]
+
+    // display selected amenities
+    content = Object.values(selectedAmenities).join(', ')
+    $('.amenities h4').html(content);
   });
 });
